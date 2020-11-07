4f4a068290198966a5c862eb61260bdae20208cb
ilja <ilja@kali.ylyxa>
2020-11-08 00:43:49 +0300

    Scripted auto-commit on change (2020-11-08 00:43:49) by gitwatch.sh

--- src/main/java/AirportsDelayCalculator.java
+++ src/main/java/AirportsDelayCalculator.java
@@ -3,20 +3,23 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
-import java.util.Arrays;
-
 public class AirportsDelayCalculator {
     public static final int ORIGIN_AIRPORT_ID_COLUMN = 11;
     public static final int DEST_AIRPORT_ID_COLUMN = 14;
     public static final int ARR_DELAY_NEW_COLUMN = 18;
-    public static final String delimiter = ",";
+    public static final String DELIMITER = ",";
+
+    private static Tuple2<Tuple2<String, String>, Float> stringToDelayPair(String s) {
+        String[] seq = s.split(DELIMITER);
+        Tuple2<String, String> first = new Tuple2<>(seq[ORIGIN_AIRPORT_ID_COLUMN], seq[DEST_AIRPORT_ID_COLUMN]);
+        Float second = Float.parseFloat(seq[ARR_DELAY_NEW_COLUMN]);
+        return new Tuple2<>(first, second);
+    }
     public static void main(String[] args) throws Exception{
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> distFile = sc.textFile("664600583_T_ONTIME_sample.csv");
-        JavaRDD<Tuple2<Tuple2<String, String>, Float>> delays = distFile.flatMap(
-                s -> new Tuple2<>(new Tuple2<>(s.split(delimiter)[ORIGIN_AIRPORT_ID_COLUMN], ))
-        );
+        JavaRDD<Tuple2<Tuple2<String, String>, Float>> delays = distFile.flatMap(AirportsDelayCalculator.stringToDelayPair);
     }
 }
