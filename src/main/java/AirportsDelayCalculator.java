1eb4eb3eadfeec68b5e1d92162480b35474eb3e2
ilja <ilja@kali.ylyxa>
2020-11-08 00:23:09 +0300

    Scripted auto-commit on change (2020-11-08 00:23:09) by gitwatch.sh

--- src/main/java/AirportsDelayCalculator.java
+++ src/main/java/AirportsDelayCalculator.java
@@ -2,12 +2,16 @@ import org.apache.spark.*;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.util.Arrays;
+
 public class AirportsDelayCalculator {
     public static void main(String[] args) throws Exception{
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> distFile = sc.textFile("664600583_T_ONTIME_sample.csv");
-        
+        JavaRDD<String> splitted = distFile.flatMap(
+                s -> Arrays.stream(.split(" ")).iterator();
+        );
     }
 }
