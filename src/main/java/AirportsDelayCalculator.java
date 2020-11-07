63f89c5e05b2c4047db946c5a40dac7e6bcd30bc
ilja <ilja@kali.ylyxa>
2020-11-08 00:13:17 +0300

    Scripted auto-commit on change (2020-11-08 00:13:17) by gitwatch.sh

--- src/main/java/AirportsDelayCalculator.java
+++ src/main/java/AirportsDelayCalculator.java
@@ -2,5 +2,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportsDelayCalculator {
-    public static void main(String[] )
+    public static void main(String[] args) throws Exception{
+
+    }
 }
