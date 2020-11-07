07cbcb7edbb36973e388e45e425aac5056beb16c
ilja <ilja@kali.ylyxa>
2020-11-08 00:33:31 +0300

    Scripted auto-commit on change (2020-11-08 00:33:31) by gitwatch.sh

--- src/main/java/AirportsDelayCalculator.java
+++ src/main/java/AirportsDelayCalculator.java
@@ -7,15 +7,18 @@ import java.util.Arrays;
 
 public class AirportsDelayCalculator {
     public static final int ORIGIN_AIRPORT_ID_COLUMN = 11;
-    public static final int DEST_AIRPORT_ID_COLUMN = 11;
-    public static final int ORIGIN_AIRPORT_ID_COLUMN = 11;
+    public static final int DEST_AIRPORT_ID_COLUMN = 14;
+    public static final int ARR_DELAY_NEW_COLUMN = 18;
+    public static final String delimiter = ",";
     public static void main(String[] args) throws Exception{
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> distFile = sc.textFile("664600583_T_ONTIME_sample.csv");
-        JavaRDD<Tuple2<String, String>, > delays = distFile.flatMap(
-                s -> Arrays.stream(s.split(",")).iterator()
+        JavaRDD<Tuple2<Tuple2<String, String>, Float>> delays = distFile.flatMap(
+                s -> {
+
+                }
         );
     }
 }
