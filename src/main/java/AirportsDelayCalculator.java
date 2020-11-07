f1ff1dfafefb8d31b92c7b72df39fba2158e62a5
ilja <ilja@kali.ylyxa>
2020-11-08 00:47:29 +0300

    Scripted auto-commit on change (2020-11-08 00:47:29) by gitwatch.sh

--- src/main/java/AirportsDelayCalculator.java
+++ src/main/java/AirportsDelayCalculator.java
@@ -1,4 +1,5 @@
 import org.apache.spark.*;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
@@ -20,6 +21,11 @@ public class AirportsDelayCalculator {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> distFile = sc.textFile("664600583_T_ONTIME_sample.csv");
-        JavaRDD<Tuple2<Tuple2<String, String>, Float>> delays = distFile.flatMap(s -> AirportsDelayCalculator.stringToDelayPair(s));
+        JavaPairRDD<Tuple2<String, String>, Float> delays = distFile.flatMap(s -> {
+            String[] seq = s.split(DELIMITER);
+            Tuple2<String, String> first = new Tuple2<>(seq[ORIGIN_AIRPORT_ID_COLUMN], seq[DEST_AIRPORT_ID_COLUMN]);
+            Float second = Float.parseFloat(seq[ARR_DELAY_NEW_COLUMN]);
+            return new Tuple2<>(first, second);
+        });
     }
 }
